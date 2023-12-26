df2060f41f9c4e1d84725d24ae608282674758c0
Ogbaje Leo Arome <leonardaromeh@gmail.com>
2023-12-26 17:34:58 +0100

    update

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-8">
     <div className="container mx-auto text-center">
       <p className="text-lg mb-4">Connect with me on social media:</p>
-      <div className="flex justify-center space-x-4">
-        <a href="#" className="text-gray-400 hover:text-white transition duration-300">
-          <i className="fab fa-twitter"></i>
-        </a>
-        <a href="#" className="text-gray-400 hover:text-white transition duration-300">
-          <i className="fab fa-linkedin"></i>
-        </a>
-        <a href="#" className="text-gray-400 hover:text-white transition duration-300">
-          <i className="fab fa-instagram"></i>
-        </a>
-      </div>
-      <p className="mt-4">&copy; 2023 Your Website. All rights reserved.</p>
+
+<div className="flex space-x-4 text-center justify-center">
+          <Link to="https://twitter.com/your_twitter" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter text-3xl hover:text-blue-400"></i>Twitter
+          </Link>
+          <Link to="https://linkedin.com/in/your_linkedin" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin-in text-3xl hover:text-blue-400"></i>Linkedin
+          </Link>
+          <Link to="https://instagram.com/your_instagram" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram text-3xl hover:text-pink-500"></i>Instagram
+          </Link>
+        </div>
+      <p className="mt-4">&copy; 2023. All rights reserved.</p>
     </div>
   </footer>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,12 +3,24 @@ import Navbar from './Navbar'
 import Footer from './footer'
 
 const Home = (props) => {
-   
+  const clients = [
+    "Leo Tech Ltd",
+    "IMT Law Dept",
+    "Client C",
+    // Add more clients as needed
+  ];
+
+  const partners = [
+    "Partner X",
+    "Partner Y",
+    "Partner Z",
+    // Add more partners as needed
+  ];
     
   return (
     <div>
       <Navbar />
-      <header className="h-screen flex items-center justify-center bg-cover" style={{ backgroundImage: 'url("https://i.ibb.co/fkR6wwz/IMG-20231226-WA0019-removebg-preview.png")' }}>
+      <header className="h-screen flex items-center justify-center bg-cover" style={{ backgroundImage: 'url("")' }}>
        <div class="text-center">
       <h1 class="text-4xl md:text-6xl font-bold mb-4">GEORGE Ukamaka Faith</h1>
       <p class="text-lg md:text-xl mb-8">Content Creator | Creative Mind | Voice Over Artiste</p>
@@ -16,36 +28,74 @@ const Home = (props) => {
     </div>
   </header>
 
-  <section className="bg-gray-100 py-16">
+  
+
+    <section className="py-16 bg-white">
       <div className="container mx-auto">
-        <h2 className="text-5xl font-semibold mb-8 text-center">About Me</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div>
-            <img
-            src="https://i.ibb.co/fkR6wwz/IMG-20231226-WA0019-removebg-preview.png"
-            border="0" 
-            alt="Profile"
-            className="w-full h-[70%] rounded-md ml-8"
-            />
-          </div>
-          <div className='p-4'>
-            <p className="text-gray-700 text-lg mb-4 text-justify">
-              Hi, I'm UKAMAKA,George Faith, a dedicated freelance content writer based in [Your Location]. With a passion for storytelling and a love for crafting compelling narratives, I bring a unique blend of creativity and expertise to every project I undertake.
-            </p>
-            <p className="text-gray-700 text-lg mb-4">
-              Over the years, I've honed my skills in [Your Niche or Specialization], producing content that not only informs but also resonates with the target audience. My writing journey has allowed me to collaborate with a diverse range of clients, from startups to established businesses, helping them communicate their brand voice effectively.
-            </p>
-            <p className="text-gray-700 text-lg mb-4">
-              Whether it's creating engaging blog posts, optimizing content for SEO, or developing captivating marketing copy, I take pride in delivering results that make an impact. I stay updated on industry trends and am committed to continuous learning to ensure that my clients receive content that meets the highest standards.
-            </p>
-            <p className="text-gray-700 text-lg">
-              Let's work together to elevate your brand through the power of words. Feel free to reach out if you're looking for a reliable and passionate content writer who can turn your ideas into compelling stories.
+        <h2 className="text-5xl font-bold mb-8 text-center">What I Offer</h2>
+        <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-2">
+          {/* Service Card 1 */}
+          <div className="bg-blue-900 p-6 rounded-md shadow-md text-white m-4">
+            <h3 className="text-xl font-semibold mb-4">Content Writing</h3>
+            <p className="text-gray-100">
+              Crafting engaging and well-researched content tailored to your audience, whether it's blog posts, articles, or website copy.
             </p>
           </div>
+
+          {/* Service Card 2 */}
+          <div className="bg-blue-900 p-6 rounded-md shadow-md text-white m-4">
+            <h3 className="text-xl font-semibold mb-4">SEO Optimization</h3>
+            <p className="text-gray-100">
+              Optimizing content for search engines to improve visibility and ranking, ensuring your content reaches a wider audience.
+            </p>
+          </div>
+
+          {/* Service Card 3 */}
+          <div className="bg-blue-900 p-6 rounded-md shadow-md text-white m-4">
+            <h3 className="text-xl font-semibold mb-4">Copywriting</h3>
+            <p className="text-gray-100">
+              Creating compelling and persuasive copy for marketing materials, advertisements, and promotional content.
+            </p>
+          </div>
+
+          {/* Service Card 4 */}
+          <div className="bg-blue-900 p-6 rounded-md shadow-md text-white m-4">
+            <h3 className="text-xl font-semibold mb-4">Social Media Content</h3>
+            <p className="text-gray-100">
+              Developing engaging and shareable content for social media platforms to enhance your online presence and connect with your audience.
+            </p>
+          </div>
+
+          {/* Add more service cards as needed */}
         </div>
       </div>
     </section>
 
+    <section className="py-16 bg-gray-100">
+      <div className="container mx-auto">
+        {/* People I Have Worked For Section */}
+        <div className="mb-12">
+          <h2 className="text-5xl font-semibold mb-4 text-center">People I Have Worked For</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+            {clients.map((client, index) => (
+              <div key={index} className="bg-white p-4 rounded-md shadow-md text-center hover:shadow-xl transition duration-300">
+                <p className="text-gray-700">{client}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Partners Section */}
+        <div>
+          <h2 className="text-3xl font-semibold mb-4">Partners</h2>
+          <ul className="list-disc pl-6">
+            {partners.map((partner, index) => (
+              <li key={index} className="text-gray-700">{partner}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
 
   <Footer />
         </div>
